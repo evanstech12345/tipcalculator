d5c47de02d30bcb9ecb4f39a727f17264b619858
evanstech12345 <evanshcode@gmail.com>
2021-06-23 23:23:53 -0400

    finishing the code

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ let tip5 = document.getElementById("5PercentTip");
 let tip10 = document.getElementById("10PercentTip");
 let tip20 = document.getElementById("20PercentTip");
 
+tip20Amount = .20;
+tip5Amount = .5;
+tip10Amount = .10;
+
 let btn = document.getElementById("btnSendTip");
 
-btn.addEventListener('click', e => {
+tip20.addEventListener('click', e => {
     let tip = document.getElementById("tipAmount").value;
-    let res = parseFloat(tip) + parseFloat(tip)
-    console.log(res)
+    let res = parseFloat(tip) * parseFloat(tip20Amount);
+    alert(res);
 })
 
+tip5.addEventListener('click', e => {
+    let tip = document.getElementById("tipAmount").value;
+    let res = parseFloat(tip) * parseFloat(tip5Amount);
+    alert(res);
+})
+
+tip10.addEventListener('click', e => {
+    let tip = document.getElementById("tipAmount").value;
+    let res = parseFloat(tip) * parseFloat(tip10Amount);
+    alert(res);
+})
